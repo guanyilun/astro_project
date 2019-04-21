6effc0153ab46d975369d35bb4e247e990385a4a
Yilun Guan <zoom.aaron@gmail.com>
2019-04-21 19:13:04 -0400

    Move common functions to utils script

--- knn_cv.py
+++ knn_cv.py
@@ -29,6 +29,8 @@ from sklearn.model_selection import GridSearchCV
 from xgboost import XGBRegressor
 from sklearn.neighbors import KNeighborsRegressor
 
+from .utils import *
+
 #####################
 # define parameters #
 #####################
@@ -49,65 +51,6 @@ parameters = {
     "leaf_size": [20,30,40,50],
     "p": [1,2],
 }
-
-#####################
-# utility functions #
-#####################
-
-def load_data(cat_filename):
-    """Loads a given number of rows from catalog and return test and train"""
-    # load catalog data
-    cat_data = pd.DataFrame(np.array(pd.read_csv(cat_filename,
-                                                 delim_whitespace=True,
-                                                 comment='#',
-                                                 header=None)))
-    # give column names
-    cat_data.columns = ['id','redshift','tu','tg','tr','ti','tz','ty',\
-                        'u10','uerr10','g10','gerr10','r10','rerr10',\
-                        'i10','ierr10','z10','zerr10','y10','yerr10']
-
-    return cat_data
-
-def prepare_data(cat_train, cat_test, use_scaler=True):
-    """This function takes in a catalog dataframe and split it into 
-    training and testing set that can be supplied to machine learning
-    models such as those in sklearn"""
-    X_train = cat_train[features_name].values
-    y_train = cat_train[labels_name].values
-
-    X_test = cat_test[features_name].values
-    y_test = cat_test[labels_name].values
-
-    # if using standard scaler
-    if use_scaler:
-        # initialize standard scaler
-        scaler = StandardScaler()
-
-        # train scaler on training data
-        scaler.fit(X_train)
-
-        # scale both train and test using this scaler
-        X_train = scaler.transform(X_train)
-        X_test = scaler.transform(X_test)
-    
-    return X_train, X_test, y_train, y_test
-
-def MAD(data, axis=0):
-    """This function calculates the median absolute deviation"""
-    absdev = np.abs(data - np.expand_dims(np.median(data, axis=axis), axis))
-    return np.median(absdev, axis=axis)/0.6745
-
-def loss_func(y_pred, y_truth):
-    """This function calculates a loss function based on truth and prediction.
-    loss = (z_photo - z_spec) / (1 + z_spec)
-    and z_photo -> y_pred
-        z_spec  -> y_truth
-    """
-    return (y_pred-y_truth)/(1+y_truth)
-
-def target_func(x, c, A, n):
-    """Target function for curve fitting"""
-    return c + A*x**n
 
 ################
 # main program #
--- rf_cv.py
+++ rf_cv.py
@@ -27,6 +27,9 @@ from scipy.optimize import curve_fit
 from sklearn.model_selection import GridSearchCV
 
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import make_scorer
+
+from .utils import *
 
 #####################
 # define parameters #
@@ -46,65 +49,6 @@ Regressor = RandomForestRegressor
 parameters = {
     "n_estimators": [10, 20, 50, 100, 200, 500, 1000],
 }
-
-#####################
-# utility functions #
-#####################
-
-def load_data(cat_filename):
-    """Loads a given number of rows from catalog and return test and train"""
-    # load catalog data
-    cat_data = pd.DataFrame(np.array(pd.read_csv(cat_filename,
-                                                 delim_whitespace=True,
-                                                 comment='#',
-                                                 header=None)))
-    # give column names
-    cat_data.columns = ['id','redshift','tu','tg','tr','ti','tz','ty',\
-                        'u10','uerr10','g10','gerr10','r10','rerr10',\
-                        'i10','ierr10','z10','zerr10','y10','yerr10']
-
-    return cat_data
-
-def prepare_data(cat_train, cat_test, use_scaler=True):
-    """This function takes in a catalog dataframe and split it into 
-    training and testing set that can be supplied to machine learning
-    models such as those in sklearn"""
-    X_train = cat_train[features_name].values
-    y_train = cat_train[labels_name].values
-
-    X_test = cat_test[features_name].values
-    y_test = cat_test[labels_name].values
-
-    # if using standard scaler
-    if use_scaler:
-        # initialize standard scaler
-        scaler = StandardScaler()
-
-        # train scaler on training data
-        scaler.fit(X_train)
-
-        # scale both train and test using this scaler
-        X_train = scaler.transform(X_train)
-        X_test = scaler.transform(X_test)
-    
-    return X_train, X_test, y_train, y_test
-
-def MAD(data, axis=0):
-    """This function calculates the median absolute deviation"""
-    absdev = np.abs(data - np.expand_dims(np.median(data, axis=axis), axis))
-    return np.median(absdev, axis=axis)/0.6745
-
-def loss_func(y_pred, y_truth):
-    """This function calculates a loss function based on truth and prediction.
-    loss = (z_photo - z_spec) / (1 + z_spec)
-    and z_photo -> y_pred
-        z_spec  -> y_truth
-    """
-    return (y_pred-y_truth)/(1+y_truth)
-
-def target_func(x, c, A, n):
-    """Target function for curve fitting"""
-    return c + A*x**n
 
 ################
 # main program #
